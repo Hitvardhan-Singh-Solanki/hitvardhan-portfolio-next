defe70d536e60630a91f9e9eed550cd84905141d
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2024-10-04 06:11:04 +0200

    work in progress

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,19 @@
+import { IoWarning } from "react-icons/io5";
 export default function Work() {
-  return <div>Work</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex items-center justify-center gap-4">
+          <div className="flex flex-col items-center justify-center">
+            <IoWarning className="text-6xl text-[--foreground]" />
+            <h2 className="text-2xl font-bold">Work In Progress</h2>
+            <p className="text-lg">
+              This page is currently under construction. Please check back
+              later.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
